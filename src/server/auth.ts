bf6a310874586dfa705828e86fbd1c265d6cbd96
FrancoARossi <rossifrancoa@gmail.com>
2023-04-09 15:29:44 -0300

    updated env vars

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -3,8 +3,9 @@ import {
   getServerSession,
   type NextAuthOptions,
   type DefaultSession,
+  type Profile,
 } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { env } from "~/env.mjs";
 import { prisma } from "~/server/db";
@@ -22,6 +23,14 @@ declare module "next-auth" {
       // ...other properties
       // role: UserRole;
     } & DefaultSession["user"];
+  }
+
+  interface Profile {
+    sub?: string;
+    name?: string;
+    email?: string;
+    image?: string;
+    email_verified?: boolean;
   }
 
   // interface User {
@@ -44,22 +53,22 @@ export const authOptions: NextAuthOptions = {
         id: user.id,
       },
     }),
+    signIn: ({ account, profile }): boolean => {
+      if (account?.provider === "google") {
+        return (profile &&
+          profile.email_verified &&
+          profile.email?.endsWith("@sirius.com.ar")) as boolean;
+      }
+      // For now only allow Google sign in
+      return false;
+    },
   },
   adapter: PrismaAdapter(prisma),
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
     }),
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
   ],
 };
 
