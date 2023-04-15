a06b48688799564b0936ba96f19620627bc25ee7
FrancoARossi <rossifrancoa@gmail.com>
2023-04-14 21:42:43 -0300

    fix layout width

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -6,7 +6,7 @@ const MainLayout = ({
   children: ReactElement | ReactElement[];
 }) => {
   return (
-    <div className="flex h-full w-full max-w-[1440px] flex-1 flex-col items-center justify-center px-20 text-center">
+    <div className="flex h-full w-full flex-1 flex-col items-center justify-center px-20 text-center">
       {children}
     </div>
   );
